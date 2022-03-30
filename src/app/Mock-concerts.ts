68ed17c3bd4b80268b5d62772657229f5284414a
qixiaoling <x.qi@novi-education.nl>
2022-03-30 20:57:33 +0200

    first commit

---
+++ src/app/Mock-concerts.ts
@@ -0,0 +1,6 @@
+import { Concert} from "./concerts";
+
+export const CONCERTS: Concert[] = [
+  { id: 1, price: 200, description: "in summer", time: new Date(), artist: "Micky", concertHall: "Amsterdam" },
+  { id: 2, price: 500, description: "in winter", time: new Date(), artist: "Minnie", concertHall: "Rotterdam" }
+];
---
+++ src/app/concerts.ts
@@ -0,0 +1,8 @@
+export interface Concert {
+  id: number,
+  price: number,
+  description: String,
+  time: Date,
+  artist: String,
+  concertHall: String
+}
