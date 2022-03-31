dd8b2db0e3e196e2fbde34da20d8d1cdd14e99a7
qixiaoling <x.qi@novi-education.nl>
2022-03-31 17:50:23 +0200

    sixth commit: route to concert-detail.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ConcertComponent} from "./concert/concert.component";
+import {ConcertDetailComponent} from "./concert-detail/concert-detail.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'concerts', component: ConcertComponent}
+  { path: 'concerts', component: ConcertComponent},
+  { path: 'concert-details/:id', component: ConcertDetailComponent}
 ];
 
 @NgModule({
---
+++ src/app/concert-detail/concert-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-concert-detail',
+  templateUrl: './concert-detail.component.html',
+  styleUrls: ['./concert-detail.component.css']
+})
+export class ConcertDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
