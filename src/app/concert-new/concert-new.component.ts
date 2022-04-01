b454217145418c6f88a953e9d79544840abcd3da
qixiaoling <x.qi@novi-education.nl>
2022-04-01 12:55:35 +0200

    create concert form 5

--- src/app/concert-new/concert-new.component.ts
+++ src/app/concert-new/concert-new.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-concert-new',
   templateUrl: './concert-new.component.html',
-  styleUrls: ['./concert-new.component.css']
+  styleUrls: ['./concert-new.component.css'],
+  styles: [`input.ng-invalid{border-left: 5px solid red;}
+    input.ng-valid{border-left: 5px solid green;}`]
 })
 export class ConcertNewComponent implements OnInit {
 
