a5243c5bfe3c23fecc2820bc764c8b536d71dd0e
qixiaoling <x.qi@novi-education.nl>
2022-04-01 14:18:26 +0200

    got artist data from backend serve, rendered in a drop down menu.

--- src/app/concert-new/concert-new.component.ts
+++ src/app/concert-new/concert-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtistService} from "../artist.service";
+import {Artist} from "../artist";
 
 @Component({
   selector: 'app-concert-new',
@@ -8,12 +10,24 @@ import { Component, OnInit } from '@angular/core';
     input.ng-valid{border-left: 5px solid green;}`]
 })
 export class ConcertNewComponent implements OnInit {
-
-  constructor() { }
+  artists: Artist[] = [];
+  constructor(private artistService : ArtistService) { }
 
   ngOnInit(): void {
+    this.getArtists();
   }
 
+  getArtists():void{
+    this.artistService.getArtists()
+      .subscribe(x => {
+        console.log(x);
+        this.artists = x;
+      })
+  }
+
+
+  ArtistId = "2";
+  ConcertHallId="3";
   onSubmit(value:any){
     console.log(value)
   }
