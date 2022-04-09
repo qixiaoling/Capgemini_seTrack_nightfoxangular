e67e6e847369146b8d24437dc559a9b0c905f2ed
qixiaoling <x.qi@novi-education.nl>
2022-04-09 12:56:46 +0200

    navbar css 1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  links? : boolean = false;
+  icon? : boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  changeLinksClasses() {
+    let className: string = "navLinks";
+    if(this.links){
+      className += " active";
+    }
+    return className;
+  }
+  closeMobileMenu() {
+    this.links = false;
+  }
+
+  changeIconClasses() {
+    let className: String = "iconLinks";
+    if(this.icon) {
+      className += " iconActive";
+    }
+    return className;
+  }
+  toggleLinks() {
+    this.links = !(this.links);
+    console.log("toggleLinks"+ this.links)
+  }
+  toggleIcon() {
+    this.icon = !(this.icon);
+    console.log("toggleIcon"+ this.icon)
+  }
+
 
 }
